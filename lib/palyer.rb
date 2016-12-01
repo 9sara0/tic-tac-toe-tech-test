076430c9e62847e76f9799a181c5c0f07281022c
Sara <sarah.alalaya@gmail.com>
2016-12-01 15:52:21 +0300

    Player: refactors if condetion into private methods

--- lib/palyer.rb
+++ lib/palyer.rb
@@ -3,8 +3,22 @@ class Player
   attr_reader :name
 
   def initialize(name)
-    raise 'Invalid palyer name' if( (name.upcase != 'O') && (name.upcase != 'X'))
+    raise 'Invalid palyer name' if ! is_valid?(name)
     @name = name.upcase
+  end
+
+  private
+
+  def is_o?(name)
+    name.upcase == 'O'
+  end
+
+  def is_x?(name)
+    name.upcase == 'X'
+  end
+
+  def is_valid?(name)
+    is_o?(name) || is_x?(name)
   end
 
 end
