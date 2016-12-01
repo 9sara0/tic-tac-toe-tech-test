5f196d7baef7e3d3039157b04b0668423e92dccf
Sara <sarah.alalaya@gmail.com>
2016-12-01 17:24:36 +0300

    Board: changed @field variable from 2-dimentional array to 1-2-dimentional of Feild objects

--- lib/board.rb
+++ lib/board.rb
@@ -1,9 +1,22 @@
+require_relative "field"
 class Board
   FIELDS_COUNT=9
   attr_reader :fields
 
   def initialize
-    @fields = Array.new(FIELDS_COUNT) { Array.new(FIELDS_COUNT)}
+    @fields = []
+    create_fields
+  end
+
+  private
+
+  def create_fields
+    x    = 0
+    rows = Math.sqrt(FIELDS_COUNT).to_i
+    while x < rows
+      rows.times { |y| @fields << Field.new(x,y) }
+      x+=1
+    end
   end
 
 end
